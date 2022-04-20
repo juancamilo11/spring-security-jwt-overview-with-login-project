1a061034d0a4fc8b7d96e19177585387fe912748
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-04-19 22:38:45 -0500

    Created base project using spring initializr

---
+++ src/main/java/dev/j3c/SpringSecurityCompleteLoginApplication.java
@@ -0,0 +1,13 @@
+package dev.j3c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCompleteLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityCompleteLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/j3c/SpringSecurityCompleteLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.j3c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCompleteLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
